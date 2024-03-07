668ecf0a55f5bf42f90df5bacaef101c4f2e9242
NekoNeko6996 <lqm231231@gmail.com>
2024-03-07 21:17:19 +0700

    .

--- js/adminPage.js
+++ js/adminPage.js
@@ -1,3 +1,74 @@
+function loadItemShowUser(userArray) {
+  var htmlComponent = `
+    <tr class="user-table-tr">
+      <th class="user-table-th">USER ID</th>
+      <th class="user-table-th">USER EMAIL</th>
+      <th class="user-table-th">USER PERMISSION</th>
+      <th class="user-table-th">USER OPTION</th>
+    </tr>
+  `;
+  userArray.forEach((row, index) => {
+    htmlComponent += `
+    <tr class="user-table-tr">
+      <td class="user-table-td">${row[0]}</td>
+      <td class="user-table-td">${row[1]}</td>
+      <td class="user-table-td">${row[2]}</td>
+      <td class="user-table-td"><button class="user-option-btn">BLOCK USER</button><button class="user-option-btn">DELETE USER</button></td>
+    </tr>`;
+  });
+  $("#user-display-table").html(htmlComponent);
+}
+
+$("document").ready(() => {
+  $.ajax({
+    url: "server/adminPage.php",
+    type: "POST",
+    data: {
+      requestCode: 1,
+      data: "",
+    },
+    success: (response) => {
+      const onloadDataResponse = JSON.parse(
+        response.replace("<!-- SERVER -->", "")
+      );
+      loadItemShowUser(onloadDataResponse.userList);
+    },
+    error: (status, error) => {
+      console.error(status, error);
+    },
+  });
+});
+
+function loadMusicData() {
+  const musicID = $("#update-music-id").val();
+  if (musicID >= 1) {
+    $.ajax({
+      url: "server/adminPage.php",
+      type: "POST",
+      data: {
+        requestCode: 3,
+        musicID,
+      },
+      success: (response) => {
+        const musicDataResponse = JSON.parse(
+          response.replace("<!-- SERVER -->", "")
+        );
+        if (musicDataResponse) {
+          $("#update-music-name").val(musicDataResponse[0][0]);
+          $("#update-music-path").val(musicDataResponse[0][1]);
+          $("#update-music-author").val(musicDataResponse[0][2]);
+          $("#update-img-path").val(musicDataResponse[0][3]);
+          $("#update-gif-path").val(musicDataResponse[0][4]);
+          $("#update-music-tag").val(musicDataResponse[0][5]);
+        }
+      },
+      error: (status, error) => {
+        console.error(status, error);
+      },
+    });
+  } else console.log("[ERROR] musicID must be greater than 0");
+}
+
 function uploadMusic(event) {
   // event.preventDefault();
   const formData = $("#uploadMusic-form").serialize();
@@ -5,7 +76,10 @@ function uploadMusic(event) {
   $.ajax({
     url: "server/adminPage.php",
     type: "POST",
-    data: formData,
+    data: {
+      requestCode: 2,
+      formData,
+    },
     success: (response) => {
       console.log(JSON.parse(response));
       window.location.reload();
--- server/adminPage.php
+++ server/adminPage.php
@@ -1,38 +1,74 @@
-
+<!-- SERVER -->
 <?php
-    $server_name = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "music_app_db";
+$server_name = "localhost";
+$username = "root";
+$password = "";
+$database = "music_app_db";
 
-    $connect = mysqli_connect($server_name, $username, $password, $database);
+$connect = mysqli_connect($server_name, $username, $password, $database);
 
-    if (!$connect) { 
-        die("[SQL] Error". mysqli_connect_error());
+if (!$connect) {
+    die("[SQL] Error" . mysqli_connect_error());
+}
+
+
+$insertQuery = "INSERT INTO music_source_path (musicName, musicPath, author, imgPath, gifPath) VALUES (?, ?, ?, ?, ?)";
+
+
+function AD_onloadQuery($DB)
+{
+    $result["userList"] = mysqli_fetch_all(mysqli_query($DB, "SELECT user.id, user.email, permission.permissionName FROM user INNER JOIN permission ON user.permissionID = permission.permissionID"));
+    $result["musicList"] = mysqli_fetch_all(mysqli_query($DB, "SELECT * FROM music_source_path LIMIT 10"));
+
+    if (!isset($result["userList"]) || !isset($result["musicList"])) {
+        die("[SQL ERROR] query error" . mysqli_error($DB));
     }
+    return $result;
+}
+
+function getMusicById($DB, $id)
+{
+    $result = mysqli_fetch_all(mysqli_query($DB, "SELECT musicName, musicPath, author, imgPath, gifPath, tag FROM music_source_path WHERE id = '$id'"));
+    if (!isset($result)) {
+        die("[SQL ERROR] query error" . mysqli_error($DB));
+    }
+    return $result;
+}
 
 
-    $insertQuery =  "INSERT INTO music_source_path (musicName, musicPath, author, imgPath, gifPath) VALUES (?, ?, ?, ?, ?)";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    switch ($_POST["requestCode"]) {
+        case 2:
+            if (isset($_POST["name-music"]) && isset($_POST["path-music"])) {
+                $nameMusic = $_POST["name-music"];
+                $pathMusic = $_POST["path-music"];
+                $author = $_POST["author-music"];
+                $imgPath = $_POST["img-path-music"];
+                $gifPath = $_POST["gif-path-music"];
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        if(isset($_POST["name-music"]) && isset($_POST["path-music"])) {
-            $nameMusic = $_POST["name-music"];
-            $pathMusic = $_POST["path-music"];
-            $author = $_POST["author-music"];
-            $imgPath = $_POST["img-path-music"];
-            $gifPath = $_POST["gif-path-music"];
-            
-            $stmt = mysqli_prepare($connect, $insertQuery);
-            mysqli_stmt_bind_param( $stmt,"sssss", $nameMusic, $pathMusic, $author, $imgPath, $gifPath);
+                $stmt = mysqli_prepare($connect, $insertQuery);
+                mysqli_stmt_bind_param($stmt, "sssss", $nameMusic, $pathMusic, $author, $imgPath, $gifPath);
 
-            $uploadResult = mysqli_stmt_execute($stmt);
-            if(!$uploadResult) {
-                echo "[SQL] Insert ERROR". mysqli_error($connect);
+                $uploadResult = mysqli_stmt_execute($stmt);
+                if (!$uploadResult) {
+                    echo "[SQL] Insert ERROR" . mysqli_error($connect);
+                }
+
+                echo json_encode(["status" => true]);
             }
+            break;
+        case 1:
+            echo json_encode(AD_onloadQuery($connect));
+            break;
 
-            echo json_encode(["status"=>true]);
-        }
+        case 3:
+            if (isset($_POST['musicID'])) {
+                echo json_encode(getMusicById($connect, $_POST["musicID"]));
+            }
+            break;
+        default:
+            break;
     }
-
+}
 ?>
